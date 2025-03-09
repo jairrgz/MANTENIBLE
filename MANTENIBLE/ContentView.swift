df6c2959ad33101d52df099c6bbfadf0ac7fff59
jairrgz27 <127373009+jairrgz@users.noreply.github.com>
2025-03-09 11:33:49 -0500

    Initial Commit

---
+++ MANTENIBLE/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MANTENIBLE
+//
+//  Created by Jorge Jair Ramirez Gaston Zuloeta on 9/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MANTENIBLE/MANTENIBLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  MANTENIBLEApp.swift
+//  MANTENIBLE
+//
+//  Created by Jorge Jair Ramirez Gaston Zuloeta on 9/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MANTENIBLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
